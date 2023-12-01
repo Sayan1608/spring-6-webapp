116c1efe0247d119b0332f37a80d1595d252bdde
Sayan Sengupta <=>
2023-12-01 22:18:45 +0530

    This is my First Commit of the spring-6-webapp.Have completed till lecture 20 along with assignment 1

---
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/Bootstrap.java
@@ -0,0 +1,72 @@
+package guru.springframework.spring6webapp.bootstrap;
+
+import guru.springframework.spring6webapp.domain.Author;
+import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.domain.Publisher;
+import guru.springframework.spring6webapp.repositories.AuthorRepository;
+import guru.springframework.spring6webapp.repositories.BookRepository;
+import guru.springframework.spring6webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bootstrap implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    private final PublisherRepository publisherRepository;
+
+    public Bootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Author auth1 = new Author();
+        auth1.setFirstName("Joseph");
+        auth1.setLastName("Murphy");
+
+        Book book1 = new Book();
+        book1.setTitle("The Power of Your Subconcious Mind");
+        book1.setIsbn("12345");
+
+        Author auth2 = new Author();
+        auth2.setFirstName("John");
+        auth2.setLastName("Grisham");
+
+        Book book2 = new Book();
+        book2.setTitle("A Painted House");
+        book2.setIsbn("5678");
+
+        Author savedAuth1 = authorRepository.save(auth1);
+        Book savedBook1 = bookRepository.save(book1);
+
+        Author savedAuth2 = authorRepository.save(auth2);
+        Book savedBook2 = bookRepository.save(book2);
+
+        auth1.getBooks().add(book1);
+        auth2.getBooks().add(book2);
+
+        authorRepository.save(auth1);
+        authorRepository.save(auth2);
+
+        Publisher pub1 = new Publisher();
+        pub1.setPublisherName("Penguin House");
+        pub1.setAddress("California, US");
+        pub1.setCity("California");
+        pub1.setState("California State");
+        pub1.setZip("000123");
+
+        Publisher savedPublisher = publisherRepository.save(pub1);
+
+        System.out.println("Inside Bootstrap Class");
+        System.out.println("Author Count : " + authorRepository.count());
+        System.out.println("Book Count : "+ bookRepository.count());
+        System.out.println("Publisher Count : " + publisherRepository.count());
+
+    }
+}
